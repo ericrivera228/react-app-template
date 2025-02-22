9e0034a9b5983f5f7fc9fb6857c784d0df11671b
Eric Rivera <ericrivera228@gmail.com>
2025-02-22 13:49:46 -0700

    Updating linting rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,22 @@
-import js from '@eslint/js'
 import globals from 'globals'
+import stylisticTs from '@stylistic/eslint-plugin-ts';
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsEslintParser from '@typescript-eslint/parser';
+
+
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     settings: { react: { version: '18.3' } },
-    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsEslintParser, 
       parserOptions: {
         project: ['./tsconfig.node.json', './tsconfig.app.json'],
         tsconfigRootDir: import.meta.dirname,
@@ -21,30 +24,103 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      '@stylistic/ts': stylisticTs,
       react,
+      '@typescript-eslint': tsEslintPlugin,
     },
     rules: {
+
+      // eslint rules
+      'sort-imports': ['error', {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      }],
+      indent: ['error', 2],
+      'semi-style': 'error',
+
+      // stylistic rules
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@stylistic/ts/semi': 'error',
+      '@stylistic/ts/space-before-blocks': 'error',
+      '@stylistic/ts/block-spacing': 'error',
+      '@stylistic/ts/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@stylistic/ts/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/ts/comma-spacing': ['error', {
+        before: false,
+        after: true, 
+      }],
+      '@stylistic/ts/function-call-spacing': ['error', 'never'],
+      '@stylistic/ts/key-spacing': ['error', { beforeColon: false }],
+      '@stylistic/ts/keyword-spacing': ['error', { before: true }],
+      '@stylistic/ts/lines-between-class-members': ['error', 'always', { exceptAfterOverload: true }],
+      '@stylistic/ts/member-delimiter-style': 'error',
+      '@stylistic/ts/no-extra-semi': 'error',
+      '@stylistic/ts/object-curly-newline': ['error',  { multiline: true }],
+      '@stylistic/ts/object-curly-spacing': ['error',  'always'],
+      '@stylistic/ts/object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
+      '@stylistic/ts/quote-props': ['error', 'as-needed'],
+      '@stylistic/ts/quotes': ['error', 'single'],
+      '@stylistic/ts/space-before-function-paren': ['error', 'never'],
+      '@stylistic/ts/space-infix-ops': 'error',
+      '@stylistic/ts/type-annotation-spacing': ['error', {
+        before: false, 
+        after: true, 
+      }],
+
+      // react rules
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
-      
-      // Typescript rules
-      'no-unused-vars': 'warn',
-      'quotes': ['error', 'single'],
-      'semi': [2, 'always'],
-
-      // React rules
-      'react/jsx-curly-newline':[ 'error', {
+      'react/jsx-curly-newline': [ 'error', {
         multiline: 'consistent',
-        singleline: 'consistent'
+        singleline: 'consistent',
       }],
       'react/button-has-type': 2,
       'react/no-array-index-key': 2,
+      'react/checked-requires-onchange-or-readonly': 'error',
+      'react/destructuring-assignment': 'error',
+      'react/hook-use-state': 'error',
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react/jsx-closing-bracket-location': 'error',
+      'react/jsx-closing-tag-location': 'error',
+      'react/jsx-curly-newline': [ 'error', {
+        multiline: 'consistent', 
+        singleline: 'consistent', 
+      } ],
+      'react/jsx-curly-spacing': [ 'error', {
+        when: 'never', 
+        allowMultiline: false, 
+      }],
+      'react/jsx-equals-spacing': ['error', 'always'],
+      'react/jsx-fragments': ['error', 'syntax'],
+      'react/jsx-indent-props': ['error', 2],
+      'react/jsx-indent': ['error', 2],
+      'react/jsx-no-constructed-context-values': 'error',
+      'react/jsx-no-useless-fragment': 'error',
+      'react/jsx-one-expression-per-line': ['error', { allow: 'single-child' }],
+      'react/jsx-props-no-multi-spaces': 'error',
+      'react/jsx-props-no-spread-multi': 'error',
+      'react/jsx-tag-spacing': ['error', {
+        closingSlash: 'never', 
+        beforeSelfClosing: 'always',
+        afterOpening: 'never',
+        beforeClosing: 'never', 
+      }],
+      'react/jsx-wrap-multilines': ['error', {
+        declaration: 'parens-new-line',
+        assignment: 'parens-new-line',
+        return: 'parens-new-line',
+        arrow: 'parens-new-line',
+        condition: 'parens-new-line',
+        logical: 'parens-new-line',
+        prop: 'parens-new-line',
+      }],
+      'react/no-array-index-key': 'error',
+      'react/no-invalid-html-attribute': 'error',
+      'react/no-multi-comp': 'error',
+      'react/no-object-type-as-default-prop': 'error',
+      'react/self-closing-comp': 'error',
+      'react/void-dom-elements-no-children': 'warn',
     },
   },
 )
